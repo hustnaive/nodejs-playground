384a4804f789ef5b0e395b7e7dfddce4346908e2
fangliang <fangl@mysoft.com.cn>
2017-02-24 02:49:20 +0800

    fix webpack demo

--- webpack/demo/src/index.js
+++ webpack/demo/src/index.js
@@ -1,1 +1,11 @@
-alert('test');
+let text = "This is a test page".split('');
+let ele = document.getElementById("text");
+
+ele.innerHTML = '';
+var timeOut = 0;
+for(let t of text) {
+	timeOut += 200;
+	setTimeout(function() {
+		ele.innerHTML += t;
+	},timeOut);
+}
